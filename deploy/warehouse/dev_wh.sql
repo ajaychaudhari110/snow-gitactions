031d379dcc03e674a7229cd3efe8895523895a94
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2025-07-31 17:47:44 +0530

    Update dev_wh.sql

--- deploy/warehouse/dev_wh.sql
+++ deploy/warehouse/dev_wh.sql
@@ -1,9 +1,1 @@
-EXECUTE IMMEDIATE $$
-DECLARE
-  wh_name VARCHAR := 'dev_wh';
-  wh_size VARCHAR := 'SMALL';
-BEGIN
-  EXECUTE IMMEDIATE FROM @dq_tutorial_db.sch.my_stage/code/single/create_warehouse.sql
-    USING (wh_name => 'dev_wh', wh_size => 'SMALL');
-END;
-$$;
+EXECUTE IMMEDIATE FROM @dq_tutorial_db.sch.my_stage/code/single/create_warehouse.sql USING (wh_name => 'dev_wh', wh_size => 'SMALL');
