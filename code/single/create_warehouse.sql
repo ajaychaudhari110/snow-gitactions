4b0cf07022e7dc1d77b2a4119f76d6b5345ee734
ajaychaudhari110 <43629795+ajaychaudhari110@users.noreply.github.com>
2025-07-31 15:08:45 +0530

    Create create_warehouse.sql

---
+++ code/single/create_warehouse.sql
@@ -0,0 +1,13 @@
+--!jinja
+DECLARE
+  sql_stmt STRING DEFAULT '';
+BEGIN
+  sql_stmt := 'CREATE WAREHOUSE IF NOT EXISTS {{wh_name}}
+    WITH
+    WAREHOUSE_SIZE = {{wh_size}}
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE';
+
+  EXECUTE IMMEDIATE :sql_stmt;
+END;
+  '
